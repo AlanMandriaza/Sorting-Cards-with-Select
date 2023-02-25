4ae2a317695480fbfc9f3a7c85ba6d3c54f7aaad
AlanMandriaza <alan.mandriaza@gmail.com>
2023-02-25 23:16:18 +0000

    codigo mas eficiente y ordenado

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const pinta = ['♦', '♥', '♠', '♣'];
-const valor = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+// Definir los valores de las cartas
+const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+// Definir las pintas de las cartas
+const pintas = [
+  { simbolo: '♦', color: 'red' },
+  { simbolo: '♥', color: 'red' },
+  { simbolo: '♠', color: 'black' },
+  { simbolo: '♣', color: 'black' }
+];
+
+// Obtener los elementos HTML
 const containerElement = document.querySelector('#container');
 const buttonElement = document.getElementById('buttonElement');
 
-buttonElement.addEventListener('click', function () {
-  const divElement = document.createElement('div');
-  
+// Agregar evento click al botón
+buttonElement.addEventListener('click', function() {
 
-  divElement.classList.add('row','card');
+  // Crear un nuevo elemento de carta
+  const cartaElement = document.createElement('div');
+  cartaElement.classList.add('row', 'card');
 
+  // Seleccionar una pinta y un valor al azar
+  const pintaAleatoria = pintas[Math.floor(Math.random() * pintas.length)];
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
 
-  const randomPinta = pinta[Math.floor(Math.random() * pinta.length)];
-  const randomValor = valor[Math.floor(Math.random() * valor.length)];
-
+  // Crear los elementos para la carta
   const arribaElement = document.createElement('div');
   arribaElement.classList.add('col', 'arriba');
-  arribaElement.textContent = randomPinta;
-
-  
-  if(randomPinta === '♦' || randomPinta === '♥') {
-    arribaElement.style.color = 'red';
-  } else if (randomPinta === '♠' || randomPinta === '♣') {
-    arribaElement.style.color = 'black';
-  }
-  
-  
+  arribaElement.textContent = pintaAleatoria.simbolo;
+  arribaElement.style.color = pintaAleatoria.color;
 
   const centroElement = document.createElement('div');
   centroElement.classList.add('col', 'centro');
-  centroElement.textContent = randomValor;
+  centroElement.textContent = valorAleatorio;
 
   const abajoElement = document.createElement('div');
   abajoElement.classList.add('col', 'abajo');
-  abajoElement.textContent = randomPinta;
+  abajoElement.textContent = pintaAleatoria.simbolo;
+  abajoElement.style.color = pintaAleatoria.color;
 
-  if(randomPinta === '♦' || randomPinta === '♥') {
-    abajoElement.style.color = 'red';
-  } else if (randomPinta === '♠' || randomPinta === '♣') {
-    abajoElement.style.color = 'black';
-  }
+  // Agregar los elementos a la carta
+  cartaElement.appendChild(arribaElement);
+  cartaElement.appendChild(centroElement);
+  cartaElement.appendChild(abajoElement);
 
-  divElement.appendChild(arribaElement);
-  divElement.appendChild(centroElement);
-  divElement.appendChild(abajoElement);
-
-  containerElement.appendChild(divElement);
+  // Agregar la carta al contenedor
+  containerElement.appendChild(cartaElement);
 });
